9f3169a2f0488ebf513fe80fb15d0361a62bb531
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-08-11 14:29:55 +0100

    Add Category to and detail page

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,24 +1,73 @@
-// import { categoryModel } from "../models/category.model.js";
-
+import { body, validationResult } from 'express-validator';
+// import async from 'async';
+import { Category } from "../models/category.model.js";
 
 const catalogController = (req, res) => {
   res.render('catalog', {title: 'Hello catalog Controller'});
 }
 
 const categoryList = (req, res) => {
-    res.render('categoryIndex',{text: 'Category Index'})
+  res.render('categoryIndex',{text: 'Category Index'})
 }
 
 const categoryDetail = (req, res) => {
-    res.send('Category Detail')
+  res.render('categoryDetail', {title: "Category Detail"});
 }
 
 const categoryCreateGet = (req, res) => {
-    res.render('categoryForm', {title:'Add Category'})
+  res.render('categoryForm', {title:'Add Category'})
 }
 
-const categoryCreatePost = (req, res) => {
-    res.send('Category Create Post')
+// const categoryCreatePost = (req, res) => {
+//   res.send('Testing Category Post');
+// }
+
+// eslint-disable-next-line no-unused-vars
+const categoryCreatePost = (req, res, next) => {
+  console.log(req.body),
+  body('name')
+  .isLength({ min: 3, max: 20 })
+  .trim()
+  .escape()
+  .required
+  body('description')
+  .isLength({ min: 10, max: 255 })
+  .trim()
+  .escape()
+  .required
+
+  console.log("I am in here anonymous function");
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    res.render('categoryForm', { title: 'Add Category', msg: 'An Error Occured', errors: errors.array() });
+    return;
+  } else {
+    console.log("I am searching if it already exist");
+    Category.findOne({name: req.body.name})
+    .exec((err, foundCategory)=> {
+      if (err) {
+        return next(err);
+      }
+      if (foundCategory) {
+        console.log("Nope It exist");
+        res.redirect(foundCategory.url)
+      } else {
+        console.log("I am saving right now");
+        const category = new Category({
+          name: req.body.name,
+          description: req.body.description
+        })
+        category.save((err) => {
+          if (err) {
+            return next(err);
+          }
+          res.redirect(category.url);
+        })
+      }
+    })
+  }
+
 }
 
 const categoryDeleteGet = (req, res) => {
--- models/category.model.js
+++ models/category.model.js
@@ -4,13 +4,14 @@ const Schema = mongoose.Schema;
 
 const categorySchema = new Schema({
   name: {type: String, require: true},
-  description: { type: String, require: true, min:3, max: 100 }
+  description: { type: String, require: true, min:3, max: 100 },
+  date: { type: Date, default: Date.now },
 });
 
 categorySchema
   .virtual('url')
-  .get(()=> {
-    return '/catalog/category'+ this._id
+  .get(function() {
+    return '/catalog/category/'+ this._id
   });
 
-export const categoryModel = mongoose.model('categoryModel', categorySchema);
+export const Category = mongoose.model('Category', categorySchema);
